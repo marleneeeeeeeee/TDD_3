d99cf0425e710e8f4884135532cb562ccc0e07f5
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-22 13:15:04 +0200

    TC_2.1 double quality decrase after sellIn_0

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -2,24 +2,32 @@ var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
   // TC_1.1:
   it("should update the quality of a normal item", function() {
-    const gildedRose = new Shop([ new Item("TC_1", 1, 1) ]);
+    const gildedRose = new Shop([ new Item("Item_1", 1, 1) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0);
   });
 
   // TC_1.2:
   it("should update the sellIn of a normal item", function() {
-    const gildedRose = new Shop([ new Item("TC_1", 1, 1) ]);
+    const gildedRose = new Shop([ new Item("Item_1", 1, 1) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(0);
   });
   
   // TC_1.3:
   it("quality is never negative", function() {
-    const gildedRose = new Shop([ new Item("TC_1", 0, 0) ]);
+    const gildedRose = new Shop([ new Item("Item_1", 0, 0) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0);
   });
+
+  // TC_2.1:
+  it("should decrease the quality twice as fast when sellIn is less than 0", function() {
+    const gildedRose = new Shop([ new Item("Item_1", -1, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(8);
+  });
+
 
 
 });
