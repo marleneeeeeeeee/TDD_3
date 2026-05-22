8a4531a92a8a4af40dd6beff9afdc7fbe1f24c82
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-22 13:01:26 +0200

    TC_1.1 update quality

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,10 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  // TC_1.1:
+  it("should update the quality of a normal item", function() {
+    const gildedRose = new Shop([ new Item("TC_1", 1, 1) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(0);
   });
 
 });
